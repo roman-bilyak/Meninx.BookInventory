925817a58975a84498ca791925fb8368ff64d8cd
Roman Bilyak <25910302+roman-bilyak@users.noreply.github.com>
2023-08-28 13:57:39 +0300

    added initial migration script

---
+++ Meninx.BookInventory.Infrastructure/Migrations/202308281054495_Initial.cs
@@ -0,0 +1,46 @@
+﻿namespace Meninx.BookInventory.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.tblBooks",
+                c => new
+                    {
+                        Id = c.Guid(nullable: false),
+                        Title = c.String(nullable: false, maxLength: 255),
+                        Author = c.String(nullable: false, maxLength: 255),
+                        ISBN = c.String(nullable: false, maxLength: 20),
+                        PublicationYear = c.Int(nullable: false),
+                        Quantity = c.Int(nullable: false),
+                        CategoryId = c.Guid(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.tblCategories", t => t.CategoryId, cascadeDelete: true)
+                .Index(t => t.CategoryId);
+            
+            CreateTable(
+                "dbo.tblCategories",
+                c => new
+                    {
+                        Id = c.Guid(nullable: false),
+                        Name = c.String(nullable: false, maxLength: 100),
+                        Description = c.String(maxLength: 255),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.tblBooks", "CategoryId", "dbo.tblCategories");
+            DropIndex("dbo.tblBooks", new[] { "CategoryId" });
+            DropTable("dbo.tblCategories");
+            DropTable("dbo.tblBooks");
+        }
+    }
+}
