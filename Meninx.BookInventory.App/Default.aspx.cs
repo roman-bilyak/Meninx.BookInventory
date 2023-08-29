cdaeb97884e05e1cc5de09210020a18b6075578e
Roman Bilyak <25910302+roman-bilyak@users.noreply.github.com>
2023-08-30 00:35:06 +0300

    added redirection to Books\List page on opening default page

--- Meninx.BookInventory.App/Default.aspx.cs
+++ Meninx.BookInventory.App/Default.aspx.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace Meninx.BookInventory.App
 {
@@ -11,7 +7,7 @@ namespace Meninx.BookInventory.App
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            Response.Redirect("Books/List.aspx");
         }
     }
 }
